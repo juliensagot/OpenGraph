a31b68ceb6232bfe66475d781752103fea01e7ea
Julien Sagot <contact@juliensagot.fr>
2020-12-29 16:23:51 +0100

    chore: fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,31 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "OpenGraph"
+    name: "OpenGraph",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "OpenGraph",
+            targets: ["OpenGraph"]
+        )
+    ],
+    dependencies: [
+//        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.0.0")
+    ],
+    targets: [
+        .target(
+            name: "OpenGraph",
+            path: "OpenGraph",
+            exclude: ["Info.plist"]
+        ),
+        // Not working because tests are not updated
+//        .testTarget(
+//            name: "OpenGraphTests",
+//            dependencies: ["OpenGraph", "OHHTTPStubs"],
+//            path: "Tests"
+//        )
+    ]
 )
